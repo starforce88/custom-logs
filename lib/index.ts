8ccc4e0906c041d2e7984d9a12e65e7912d1c9ab
Ivan Lozada <ivan@amarello.com.mx>
2019-08-27 20:39:42 -0500

    Log terminado v1

--- lib/index.ts
+++ lib/index.ts
@@ -1,4 +1,40 @@
-export const fun = (a) => {
-    return a;
+import {Logging} from '@google-cloud/logging';
+
+export const logs = (nombre, severity, mensaje, tipo) => {
+    if(process.env.NODE_ENV == 'production'){
+        let logging = new Logging({
+            projectId: process.env.GOOGLE_CLOUD_PROJECT,
+        });
+        let log = logging.log(nombre);
+        let metadata = {
+            resource: {
+                type: tipo
+            },
+            severity: severity
+        };
+
+        let entry = log.entry(metadata, mensaje);
+        log.write(entry).then(() => {
+            console.log('LOG GUARDADO_l');
+        }).catch((error) => {
+            console.log('ERROR');
+            console.log(error);
+        });
+    }else{
+        let log = {
+            logName: `projects/${process.env.GOOGLE_CLOUD_PROJECT}/logs/${nombre}`,
+            jsonPayload: mensaje,
+            receiveTimestamp: new Date(),
+            resource: {
+                labels: {
+                    project_id: process.env.GOOGLE_CLOUD_PROJECT
+                },
+                type: tipo
+            }
+        }
+        console.log(log);
+    }
+    return;
 }
 
+//logs('MODULO-PRUEBA',0,{nuevo: 'dasdsadsa', otro: {a: 'ddddd'}}, 'gae_app');
